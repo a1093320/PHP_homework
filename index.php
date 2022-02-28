b93af4365b5b9d1fa02e95d6104899ecf9483b57
a1093320 <100362136+a1093320@users.noreply.github.com>
2022-02-28 21:41:54 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+</head>
+<body>
+<?php
+    $a = 1;
+    $b = 2;
+    echo "<b>Hello</b> PHP!";
+    echo "</br>";
+    echo date ("Y,m,d");
+    echo "</br>";
+    echo "$a+$b=";
+    echo $a+$b;
+?>
+</body>
+</html>
