8899fcde3524c135aa3a560c63a78f04a586cf09
a1093320 <100362136+a1093320@users.noreply.github.com>
2022-03-24 01:46:46 +0800

    Add files via upload

---
+++ fail.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>Login Failed</title>
+</head>
+<body>
+<center>
+    <div>
+        <center><font size=40 face="標楷體" color="red"><p>登入失敗<p></font>
+    </div>
+    <div>
+        <form action="signup.php" method="post" enctype="multipart/form-data">
+        <input type="submit" name="submit" value="前往註冊">&nbsp<input type="button" name="login" value="重新登入">
+        </form>
+    </div>
+</center>
+</body>
+</html>
---
+++ info.php
@@ -0,0 +1,9 @@
+<?php
+    $username=$_POST["user"];
+    $password=$_POST["password"];
+    $email=$_POST["email"];
+    $tel=$_POST["tel"];
+    echo "您的帳號:".$username."</br>";
+    echo "您的密碼:".$password."</br>";
+    echo "您的email:".$email."</br>";
+    echo "您的電話:".$tel."</br>";
